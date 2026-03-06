b10ef0c5798af406c586f39bcd052e6c7a4aa622
PCGosia <malgorzataserafinlatka@gmail.com>
2026-03-06 20:24:16 +0100

    Add current weatcher.

--- src/components/WeatherCard.vue
+++ src/components/WeatherCard.vue
@@ -1,12 +1,31 @@
 <template>
-  <div class="weather-container">
+  <div class="weather_container">
     <div class="weather-wrap">
       <div class="search-box">
-      <div class="city-name">
-        <h2>{{ selectedCity.name }}</h2>
-      </div>
-      <InputComponent @events="handleInputEvent"/>
-      <CurrentWeather @events="handleInputEvent" />
+        <div class="city-name">
+          <h2>{{ selectedCity.name }}</h2>
+        </div>
+        <InputComponent @events="handleInputEvent" />
+        <div class="current_weather">
+          <h2 class="today_date"></h2>
+          <div class="openweather_icon">
+
+            <div class="flex_col">
+
+            </div>
+          </div>
+          <div class="weather_details">
+            <div class="flex_row">
+
+              <p>Humidity</p>
+            </div>
+            <div class="flex_row">
+
+
+            </div>
+          </div>
+        </div>
+        <CurrentWeather @events="handleInputEvent" />
         <EchartComponent :options="chartOptions" />
       </div>
     </div>
@@ -30,7 +49,7 @@ const InputComponent = defineAsyncComponent(() => import('./InputComponent.vue')
 const CurrentWeather = defineAsyncComponent(() => import('./CurrentWeather.vue'));
 
 const chartOptions = computed(() => ({
-   xAxis: {
+  xAxis: {
     type: 'category',
     boundaryGap: false,
     data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
@@ -63,7 +82,49 @@ const handleInputEvent = async (selectedCity) => {
 </script>
 
 <style scoped>
-.weather-container {
+.weather_container {
   padding: 20px;
+}
+
+.today_date {
+  font-size: 14px;
+  padding: 2px;
+}
+
+.openweather_icon {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 5px;
+  padding: 5px;
+}
+
+.weather_details {
+  display: flex;
+  justify-content: space-between;
+  gap: 10px;
+  font-size: 12px;
+  padding: 1rem;
+  margin-top: 5px;
+  margin-bottom: 16px;
+}
+
+.weather_details img {
+  max-width: 2rem;
+  filter: invert(100%);
+}
+
+.flex_col {
+  flex-direction: column;
+}
+
+.flex_row {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+}
+
+.temp_degree {
+  font-size: 2rem;
 }
 </style>
